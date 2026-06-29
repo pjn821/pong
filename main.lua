8e9d18fb8fc09a61ef527f9b412ce90bd5d09810
pjn <paulomarcal771@gmail.com>
2026-06-29 04:07:41 +0100

    second update

--- main.lua
+++ main.lua
@@ -10,12 +10,18 @@ paddle1y = virtual_height/2-5
 
 paddle2x = virtual_width/2+210
 paddle2y = virtual_height/2-5
-
-ballx = virtual_width/2-2
-bally = virtual_height/2-2
+ball= {x = virtual_width/2-2, y = virtual_height/2-2, width = 5, height = 5}
+ballx = ball.x
+bally = ball.y
 
 balldx = -100
 balldy = 100
+
+pontosPlayer1 = 0
+pontosPlayer2 = 0
+
+
+estadoJogo = 'start'
 
 push = require 'push'
 
@@ -33,75 +39,103 @@ end
 
 function love.update(dt)
 
-    if love.keyboard.isDown('w') and paddle1y ~= virtual_height-1 then
+    --  Movimentação das raquetes 
+    if love.keyboard.isDown('w') then
         paddle1y = paddle1y - 200 * dt
     end
-    if love.keyboard.isDown('s') and paddle1y ~= 0 then
+    if love.keyboard.isDown('s') then
         paddle1y = paddle1y + 200 * dt
     end
 
-    if love.keyboard.isDown('up') and paddle2y ~= virtual_height-1 then
+    if love.keyboard.isDown('up') then
         paddle2y = paddle2y - 200 * dt
     end
-    if love.keyboard.isDown('down') and paddle2y ~= 0 then
+    if love.keyboard.isDown('down') then
         paddle2y = paddle2y + 200 * dt
     end
 
-
-
-    --delimitador vertical do paddle1 and paddle2
+    --  Delimitadores verticais das raquetes 
     if paddle1y < 0 then
-        paddle1y = 0 -200 * dt
-    end
-
-    if paddle1y > 216 then
-        paddle1y = 216 + 200 * dt
+        paddle1y = 0
+    elseif paddle1y > virtual_height - 25 then
+        paddle1y = virtual_height - 25
     end
 
     if paddle2y < 0 then
-        paddle2y = 0 -200 * dt
+        paddle2y = 0
+    elseif paddle2y > virtual_height - 25 then
+        paddle2y = virtual_height - 25
     end
 
-    if paddle2y > 216 then
-        paddle2y = 216 + 200 * dt
-    end
-
-    --ball movement
-    ballx = ballx + balldx * dt
-    bally = bally + balldy * dt
-
-    --delimitador vertical bally
-    
-    if bally<= 0 then 
+    --  Delimitador vertical da bola 
+    if bally <= 0 then 
+        bally = 0
+        balldy = balldy * -1
+    elseif bally >= virtual_height - 5 then
+        bally = virtual_height - 5
         balldy = balldy * -1
     end
 
-    if bally >= virtual_height then
-        balldy = balldy * -1
+    -- movimentação da bola
+    if estadoJogo == 'play' then
+        ballx = ballx + balldx * dt
+        bally = bally + balldy * dt
     end
 
-    --delimitador horizontal ballx    --delimitador horizontal ballx
-if ballx <= paddle1x and balldx < 0 then
-    print("BATEU NA RAQUETE 1!") -- Isto vai escrever na sua consola de comandos
-    balldx = balldx * -1
-end
+    function checkColisao(ball, paddle)
+        if ball.x < paddle.x + paddle.width and
+           ball.x + ball.width > paddle.x and
+           ball.y < paddle.y + paddle.height and
+           ball.y + ball.height > paddle.y then
+            return true
+        end
+        return false
+    end
 
-
-    if ballx >= paddle2x and balldx > 0 then
-        print("BATEU NA RAQUETE 2!")
+    -- Colisão com Raquete 1 (Esquerda)
+    if checkColisao({x = ballx, y = bally, width = 5, height = 5}, {x = paddle1x, y = paddle1y, width = 5, height = 25}) then
         balldx = balldx * -1
+        ballx = paddle1x + 5 
+    end
+
+    -- Colisão com Raquete 2 (Direita)
+    if checkColisao({x = ballx, y = bally, width = 5, height = 5}, {x = paddle2x, y = paddle2y, width = 5, height = 25}) then
+        balldx = balldx * -1
+        ballx = paddle2x - 5 
+    end
+
+
+    function resetBall()
+        ballx = virtual_width / 2 - 2
+        bally = virtual_height / 2 - 2
+        balldx = -balldx 
+        balldy = 100
+    end
+
+    --  Pontuação
+    if ballx < 0 then
+        pontosPlayer2 = pontosPlayer2 + 1
+        resetBall()
+    end
+
+    if ballx > virtual_width then
+        pontosPlayer1 = pontosPlayer1 + 1
+        resetBall()
     end
 
 end
+
 
 
 function love.draw()
     
     push.start()
-    love.graphics.printf("hello pong", 0, virtual_height/2-110,virtual_width , 'center')
+    love.graphics.printf("PONG", 0, virtual_height/2-110,virtual_width , 'center')
     love.graphics.rectangle('fill', ballx, bally, 5, 5)
     love.graphics.rectangle('fill', paddle1x, paddle1y, 5, 25)
     love.graphics.rectangle('fill', paddle2x, paddle2y, 5, 25)
+    love.graphics.printf("Player 1", -150, 20, virtual_width, 'center')
+    love.graphics.printf("Player 2", 150, 20, virtual_width, 'center')
     push.finish()
 end
 
@@ -111,5 +145,16 @@ end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    end
+
+    if key == 'enter' or key == 'return' then
+        if estadoJogo == 'start' then
+            estadoJogo = 'play'
+        else
+            estadoJogo = 'start'
+            pontosPlayer1 = 0
+            pontosPlayer2 = 0
+            resetBall()
+        end
     end
 end
